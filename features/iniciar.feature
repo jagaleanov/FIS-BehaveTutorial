88565f427d8ad0edf6a2781cd00bfb078490c60c
jagaleanov <jagaleanov@correo.udistrital.edu.co>
2020-12-13 11:36:22 -0500

    commit

---
+++ features/iniciar.feature
@@ -0,0 +1,10 @@
+Feature: iniciar juego
+    
+    Como repartidor quiero entregar 2 cartas a cada jugador para iniciar el juego.
+
+Scenario: iniciar juego
+Given el inicio del juego
+Then la mano del jugador se inicia con 2 cartas
+And la mano del repartidor se inicia con 2 cartas
+And es el turno del jugador
+
---
+++ features/steps/steps_iniciar.py
@@ -0,0 +1,19 @@
+from behave import *
+from juego import Juego
+
+
+@given('el inicio del juego')
+def implementacion(context):
+    context.juego = Juego()
+
+@then('la mano del jugador se inicia con 2 cartas')
+def implementacion(context):
+    assert len(context.juego.mano_jugador.cartas) == 2
+
+@then('la mano del repartidor se inicia con 2 cartas')
+def implementacion(context):
+    assert len(context.juego.mano_repartidor.cartas) == 2
+
+@then('es el turno del jugador')
+def implementacion(context):
+    assert context.juego.turno.turno_de_jugador == True
